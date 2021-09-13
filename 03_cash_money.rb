c1ee9f615951950c51363098952c19557594b69b
AvelyV <avely.voore@gmail.com>
2021-09-13 17:48:54 +1000

    ch 3

--- 03_cash_money.rb
+++ 03_cash_money.rb
@@ -1,8 +1,8 @@
 # Write a method called calculate_tax_payable which takes a single argument called income.
 
-# Use the tax brackets shown below to calculate tax payable on income, 
-# and then calculate the tax payable as a percentage of the income. 
-# The results should be returned in a hash containing a key called percentage_of_income 
+# Use the tax brackets shown below to calculate tax payable on income,
+# and then calculate the tax payable as a percentage of the income.
+# The results should be returned in a hash containing a key called percentage_of_income
 # and a key called tax_payable.
 
 # Tax brackets:
@@ -15,8 +15,38 @@
 
 # {:percentage_of_income=>30.13722855508853, :tax_payable=>54277.45}
 
+def calculate_tax_payable(income)
+  x = { percentage_of_income: 0, tax_payable: 0 }
+
+  if income <= 18_200
+  elsif income > 18_200 && income <= 37_000
+    x[:tax_payable] = (income - 18_200) * 0.19
+  elsif income > 37_000 && income <= 87_000
+    x[:tax_payable] = 3572 + ((income - 37_000) * 0.325)
+  elsif income > 87_000 && income <= 180_000
+    x[:tax_payable] = 19_822 + ((income - 87_000) * 0.37)
+  else
+    x[:tax_payable] = 54_232 + ((income - 180_000) * 0.45)
+  end
+  x[:percentage_of_income] = x[:tax_payable] * 100 / income
+  x
+end
+
+def save_to_file(tax_info)
+    puts "You have #{tax_info[:tax_payable]} tax payable"
+    puts "You will be taxed #{tax_info[:percentage_of_income]}%"
+end
+
+
+save_to_file(calculate_tax_payable(180_101))
+foo = calculate_tax_payable(123)
+save_to_file(calculate_tax_payable(180_101))
+
+
+
+
 # After correctly returning the right amounts, make a new method called save_to_file which
 # takes a single argument which is the hash returned by calculate_tax_payable
 
-# this method is supposed to save to a text file the person's name, 
+# this method is supposed to save to a text file the person's name,
 # their percentage_of_income and their tax_payable in an easy to read format.
